befc94016bb2d9404878d0060a2598136adde51c
Catalina Ernu <catalina.ernu@gmail.com>
2023-12-05 13:42:26 +0200

    added ArrayStack class

---
+++ PythonLab3/array_stack.py
@@ -0,0 +1,35 @@
+from stack_interface import Stack
+
+
+class ArrayStack(Stack):
+    def __init__(self):
+        self.array = []
+        self.top = -1
+        self.MAX_SIZE = 9
+
+    def push(self, element):
+        if self.is_full():
+            print(f"Stack is full. Cannot push element: {element}")
+            return
+        self.top += 1
+        self.array.append(element)
+
+    def pop(self):
+        if self.is_empty():
+            print("Stack is empty. Cannot pop element.")
+            return None
+        popped = self.array.pop()
+        self.top -= 1
+        return popped
+
+    def peek(self):
+        if self.is_empty():
+            print("Stack is empty. No element to peek.")
+            return None
+        return self.array[self.top]
+
+    def is_empty(self):
+        return self.top == -1
+
+    def is_full(self):
+        return self.top == self.MAX_SIZE - 1

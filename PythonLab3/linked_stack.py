2660666849b5cda5e07de1ce26ff8725013f044c
Catalina Ernu <catalina.ernu@gmail.com>
2023-12-05 13:44:45 +0200

    added Node class & LinkedStack class

---
+++ PythonLab3/linked_stack.py
@@ -0,0 +1,44 @@
+from stack_interface import Stack
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedStack(Stack):
+    def __init__(self):
+        self.top = None
+        self.size = 0
+        self.MAX_SIZE = 9
+
+    def push(self, element):
+        if self.is_full():
+            print(f"Stack is full. Cannot push element: {element}")
+            return
+        new_node = Node(element)
+        new_node.next = self.top
+        self.top = new_node
+        self.size += 1
+
+    def pop(self):
+        if self.is_empty():
+            print("Stack is empty. Cannot pop element.")
+            return None
+        popped = self.top.data
+        self.top = self.top.next
+        self.size -= 1
+        return popped
+
+    def peek(self):
+        if self.is_empty():
+            print("Stack is empty. No element to peek.")
+            return None
+        return self.top.data
+
+    def is_empty(self):
+        return self.size == 0
+
+    def is_full(self):
+        return self.size == self.MAX_SIZE
